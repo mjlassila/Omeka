3d74a6ee003de87add7baa5ab197063feace7eda
Patrick Murray-John <patrickmjchnm@gmail.com>
2011-11-28 12:40:37 -0500

    Changes helper show_file_metadata to  not use Elements

--- application/helpers/FileFunctions.php
+++ application/helpers/FileFunctions.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * All File helper functions
- * 
+ *
  * @copyright Roy Rosenzweig Center for History and New Media, 2007-2010
  * @license http://www.gnu.org/licenses/gpl-3.0.txt
  * @package Omeka_ThemeHelpers
@@ -26,7 +26,7 @@
   * Displays a set of files based on the file's MIME type and any options that are
   * passed.  This is primarily used by other helper functions and will not be used
   * by theme writers in most cases.
-  * 
+  *
   * @since 0.9
   * @uses Omeka_View_Helper_Media
   * @param array $files An array of File records to display.
@@ -36,7 +36,7 @@
   * div.
   * @return string HTML
   */
- function display_files($files, array $props = array(), $wrapperAttributes = array('class'=>'item-file')) 
+ function display_files($files, array $props = array(), $wrapperAttributes = array('class'=>'item-file'))
  {
      require_once 'Media.php';
      $helper = new Omeka_View_Helper_Media;
@@ -58,7 +58,7 @@
  
  /**
   * Retrieve metadata for a given field in the current file.
-  * 
+  *
   * @since 1.0
   * @param string $elementSetName
   * @param string $elementName
@@ -86,9 +86,13 @@
 
  /**
   * Retrieve the set of all metadata for the current file.
-  * 
+  *
   * @since 1.0
   * @param array $options Optional
+  *  Available options:
+  *  - 'show_element_sets' => array List of id3 keys in audio and/or video
+  *    misleading key name is here for backward compatibility to when we used Elements for this data
+  *  - 'return_type' => string 'array', 'html'.  Defaults to 'html'.
   * @param File|null $file Optional
   * @return string|array
   */
@@ -97,17 +101,66 @@
      if (!$file) {
          $file = get_current_file();
      }
+     
+     $metadata = unserialize($file->metadata);
+     if(isset($options['return_type']) && $options['return_type'] = 'array') {
+         $returnArray = array();
+         if(isset($metadata['audio']) && empty($options['show_element_sets'])) {
+             $returnArray['audio'] = $metadata['audio'];
+         } else {
+             $returnArray['audio'] = array();
+             foreach($options['show_element_sets'] as $field) {
+                 if(isset($metadata['audio'][$field])) {
+                     $returnArray['audio'][$field] = $metadata['audio'][$field];
+                 }
+             }
+         }
+         if(isset($metadata['video']) && empty($options['show_element_sets'])) {
+             $returnArray['video'] = $metadata['video'];
+         } else {
+             $returnArray['video'] = array();
+             foreach($options['show_element_sets'] as $field) {
+                 if(isset($metadata['video'][$field])) {
+                     $returnArray['video'][$field] = $metadata['video'][$field];
+                 }
+             }
+         }
+         return $returnArray;
+     }
+     
+     $html = "";
+     if(isset($metadata['audio'])) {
+         $html .= "<div class='file-metadata'>";
+         $html .= "<h2>Audio Metadata</h2>";
+         $html .= "<dl>";
+         foreach($metadata['audio'] as $field=>$value) {
+             $html .= "<dt>$field</dt><dd>$value</dd>";
+         }
+         $html .= "</dl>";
+     }
+     
+      if(isset($metadata['video'])) {
+         $html .= "<div class='file-metadata'>";
+         $html .= "<h2>Image/Video Metadata</h2>";
+         $html .= "<dl>";
+         foreach($metadata['video'] as $field=>$value) {
+             $html .= "<dt>$field</dt><dd>$value</dd>";
+         }
+         $html .= "</dl>";
+     }
+     $html .= "</div>";
+     return $html;
      return __v()->fileMetadataList($file, $options);
  }
  
   /**
   * Returns the most recent files
-  * 
+  *
   * @since 1.1
   * @param integer $num The maximum number of recent files to return
   * @return array
   */
- function recent_files($num = 10) 
+ function recent_files($num = 10)
  {
      return get_files(array('recent'=>true), $num);
  }
@@ -134,7 +187,7 @@
 
  /**
  * Retrieve the set of files for the current loop.
- * 
+ *
  * @since 1.1
  * @return array
  */
@@ -145,7 +198,7 @@
 
  /**
  * Loops through files assigned to the view.
- * 
+ *
  * @since 1.1
  * @return mixed The current file in the loop.
  */
@@ -156,13 +209,13 @@
 
  /**
  * Determine whether or not there are any files in the database.
- * 
+ *
  * @since 1.1
  * @return boolean
  */
  function has_files()
  {
-    return (total_files() > 0);    
+    return (total_files() > 0);
  }
 
  /**
