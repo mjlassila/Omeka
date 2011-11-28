132f305576f529a24c3a4ad1e215c08a33529e6e
Patrick Murray-John <patrickmjchnm@gmail.com>
2011-11-28 15:24:53 -0500

    Sets up the migration.

---
+++ application/migrations/20111104000000_removeFileInfo.php
@@ -0,0 +1,40 @@
+<?php
+
+class removeFileInfo extends Omeka_Db_Migration
+{
+    public function up()
+    {
+        $db = $this->getDb();
+
+        $db->query("ALTER TABLE `{$db->File}` ADD `metadata` TEXT NULL ");
+        $db->query("DROP TABLE IF EXISTS `{$db->MimeElementSetLookup}`");
+        
+        //remove Omeka File Image adn Omeka File Video element sets, elements, and texts
+        $imageElements = $db->getTable('Element')->findBySet('Omeka Image File');
+        $videoElements = $db->getTable('Element')->findBySet('Omeka Video File');
+        
+        foreach($imageElements as $element) {
+            $element->delete();
+        }
+        foreach($videoElements as $element) {
+            $element->delete();
+        }
+        $imageElementSet = $db->getTable('ElementSet')->findByName('Omeka Image File');
+        $videoElementSet = $db->getTable('ElementSet')->findByName('Omeka Video File');
+        $imageElementSet->delete();
+        $videoElementSet->delete();
+        
+        //go through all the existing files and redo grabbing the metadata with the new process
+        
+    }
+
+    public function down()
+    {
+        $db = $this->getDb();
+
+        $db->query("ALTER TABLE `{$db->File}` DROP `metadata`");
+    }
+    
+    
+    
+}
